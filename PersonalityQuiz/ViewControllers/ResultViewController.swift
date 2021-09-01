52efba334bc0b46238e0e38cf87d914157b5e7d5
Alex <arcani91@gmail.com>
2021-09-01 23:02:44 +0400

    Added minor corrections.

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -19,32 +19,30 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         showAnimalType()
-        showAnimalDescription()
         navigationBar.hidesBackButton = true
     }
     
     private func showAnimalType() {
-        guard let shownAnimalEmoji = defineTestResults()?.rawValue else { return }
+        guard let shownAnimalEmoji = defineTestResults()?.rawValue,
+              let shownAnimalDescription = defineTestResults()?.definition
+        else { return }
         animalTypeDeclarationLabel.text = "Вы - \(shownAnimalEmoji)"
-    }
-    
-    private func showAnimalDescription() {
-        guard let shownAnimalDescription = defineTestResults()?.definition else { return }
         animalDescriptionLabel.text = shownAnimalDescription
     }
     
     private func defineTestResults() -> Animal? {
-        var answersDict: [Animal : Int] = [:]
+        var answersDictionary: [Animal : Int] = [:]
         var result: Animal
         
         chosenAnswers.forEach {
-            if let count = answersDict[$0.animal] {
-                answersDict[$0.animal] = count + 1
+            if let count = answersDictionary[$0.animal] {
+                answersDictionary[$0.animal] = count + 1
             } else {
-                answersDict[$0.animal] = 1
+                answersDictionary[$0.animal] = 1
             }
         }
-        guard let resultingAnimal = answersDict
+        
+        guard let resultingAnimal = answersDictionary
                 .sorted(by: { $0.value > $1.value })
                 .first?.key
         else { return nil }
